5294cbee7fa1e18334c5209fddf80f7a763ae837
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-01-29 19:54:59 +0100

    lista de tareas creada en datod.js

---
+++ js/datos.js
@@ -0,0 +1,31 @@
+let listaTareas = new Array();
+
+listaTareas = [
+    
+    {
+        'idTarea': 0,
+        'titulo': 'Estudia JavaScript',
+        'prioridad':'urgente',
+    },
+    {
+        'idTarea': 1,
+        'titulo': 'Pasear al perro',
+        'prioridad':'diaria',
+    },
+    {
+        'idTarea': 2,
+        'titulo': 'Salir a comer',
+        'prioridad':'optativo',
+    },
+    {
+        'idTarea': 3,
+        'titulo': 'Pasar al banco',
+        'prioridad':'urgente',
+    },
+    {
+        'idTarea': 4,
+        'titulo': 'Ver la tele',
+        'prioridad':'optativo',
+    }
+
+]
